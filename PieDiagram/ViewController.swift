d43d908403f6b1bea11b6c1a8d7d9c3882d959d7
Ihor Stasiv <>
2021-11-30 17:41:11 +0200

    replace old image to new

--- PieDiagram/ViewController.swift
+++ PieDiagram/ViewController.swift
@@ -10,58 +10,19 @@ import CoreGraphics
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diagramContainerView: UIView!
+    @IBOutlet private var mainBackgroundView: UIView!
+    @IBOutlet private var diagramContainerView: UIView!
     
-    @IBOutlet weak var firstBlockImageView: UIImageView!
-    @IBOutlet weak var secondBlockImageView: UIImageView!
-    @IBOutlet weak var thirdBlockImageView: UIImageView!
-    @IBOutlet weak var fourBlockImageView: UIImageView!
-    @IBOutlet weak var fiveBlockImageView: UIImageView!
-    @IBOutlet weak var sixBlockImageView: UIImageView!
-    @IBOutlet weak var sevenBlockImageView: UIImageView!
-    @IBOutlet weak var eightBlockImageView: UIImageView!
-    @IBOutlet weak var nineBlockImageView: UIImageView!
-    @IBOutlet weak var tenBlockImageView: UIImageView!
-    
-    @IBOutlet weak var firstContainerView: UIView!
-    @IBOutlet weak var firstTitleView: UILabel!
-    @IBOutlet weak var firstSlider: UISlider!
-    
-    @IBOutlet weak var secondContainerView: UIView!
-    @IBOutlet weak var secondTitleLabel: UILabel!
-    @IBOutlet weak var secondSlider: UISlider!
-    
-    @IBOutlet weak var thirdContainerView: UIView!
-    @IBOutlet weak var TitleLabel: UILabel!
-    @IBOutlet weak var thirdSlider: UISlider!
-    
-    @IBOutlet weak var fourContainerView: UIView!
-    @IBOutlet weak var fourTitleLabel: UILabel!
-    @IBOutlet weak var fourSlider: UISlider!
-    
-    @IBOutlet weak var fiveContainerView: UIView!
-    @IBOutlet weak var fiveTitleLabel: UILabel!
-    @IBOutlet weak var fiveSlider: UISlider!
-    
-    @IBOutlet weak var sixContainerView: UIView!
-    @IBOutlet weak var sixTitleLabel: UILabel!
-    @IBOutlet weak var sixSlider: UISlider!
-    
-    @IBOutlet weak var sevenContainerView: UIView!
-    @IBOutlet weak var sevenTitleLabel: UILabel!
-    @IBOutlet weak var sevenSlider: UISlider!
-    
-    @IBOutlet weak var eightContainerView: UIView!
-    @IBOutlet weak var eightTitleLabel: UILabel!
-    @IBOutlet weak var eightSlider: UISlider!
-    
-    @IBOutlet weak var nineContainerView: UIView!
-    @IBOutlet weak var nineTitleLabel: UILabel!
-    @IBOutlet weak var nineSlider: UISlider!
-    
-    @IBOutlet weak var tenContainerView: UIView!
-    @IBOutlet weak var tenTitleLabel: UILabel!
-    @IBOutlet weak var tenSlider: UISlider!
+    @IBOutlet private var firstBlockImageView: UIImageView!
+    @IBOutlet private var secondBlockImageView: UIImageView!
+    @IBOutlet private var thirdBlockImageView: UIImageView!
+    @IBOutlet private var fourBlockImageView: UIImageView!
+    @IBOutlet private var fiveBlockImageView: UIImageView!
+    @IBOutlet private var sixBlockImageView: UIImageView!
+    @IBOutlet private var sevenBlockImageView: UIImageView!
+    @IBOutlet private var eightBlockImageView: UIImageView!
+    @IBOutlet private var nineBlockImageView: UIImageView!
+    @IBOutlet private var tenBlockImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -73,36 +34,46 @@ class ViewController: UIViewController {
         setupDiagramContainerView()
     }
     
+    private func addTopBackGroundLayer() {
+        mainBackgroundView.layoutIfNeeded()
+        let halfWidth = mainBackgroundView.frame.height / 2
+        let topLeftPoint = CGPoint(x: mainBackgroundView.frame.origin.x, y: mainBackgroundView.frame.origin.y)
+        let topRightPoint = CGPoint(x: mainBackgroundView.frame.width + topLeftPoint.x, y: topLeftPoint.y)
+        let midleLeftPoint = CGPoint(x: topLeftPoint.x, y: halfWidth)
+        let midleRightPoint = CGPoint(x: topRightPoint.x, y: halfWidth)
+        
+    }
+    
     private func setupDiagramContainerView() {
         self.diagramContainerView.layoutIfNeeded()
         self.diagramContainerView.layer.cornerRadius = diagramContainerView.frame.size.height / 2
     }
     
     private func changeImageAction(slider: UISlider) {
-        switch slider {
-        case firstSlider:
-            choseDiagramImagee(fill: slider.value, imageView: firstBlockImageView, name: "one")
-        case secondSlider:
-            choseDiagramImagee(fill: slider.value, imageView: secondBlockImageView, name: "two")
-        case thirdSlider:
-            choseDiagramImagee(fill: slider.value, imageView: thirdBlockImageView, name: "three")
-        case fourSlider:
-            choseDiagramImagee(fill: slider.value, imageView: fourBlockImageView, name: "four")
-        case fiveSlider:
-            choseDiagramImagee(fill: slider.value, imageView: fiveBlockImageView, name: "five")
-        case sixSlider:
-            choseDiagramImagee(fill: slider.value, imageView: sixBlockImageView, name: "six")
-        case sevenSlider:
-            choseDiagramImagee(fill: slider.value, imageView: sevenBlockImageView, name: "seven")
-        case eightSlider:
-            choseDiagramImagee(fill: slider.value, imageView: eightBlockImageView, name: "eight")
-        case nineSlider:
-            choseDiagramImagee(fill: slider.value, imageView: nineBlockImageView, name: "nine")
-        case tenSlider:
-            choseDiagramImagee(fill: slider.value, imageView: tenBlockImageView, name: "ten")
-        default:
-            break
-        }
+//        switch slider {
+//        case firstSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: firstBlockImageView, name: "One")
+//        case secondSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: secondBlockImageView, name: "Two")
+//        case thirdSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: thirdBlockImageView, name: "Three")
+//        case fourSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: fourBlockImageView, name: "Four")
+//        case fiveSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: fiveBlockImageView, name: "Five")
+//        case sixSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: sixBlockImageView, name: "Six")
+//        case sevenSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: sevenBlockImageView, name: "Seven")
+//        case eightSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: eightBlockImageView, name: "Eight")
+//        case nineSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: nineBlockImageView, name: "Nine")
+//        case tenSlider:
+//            choseDiagramImagee(fill: slider.value, imageView: tenBlockImageView, name: "Ten")
+//        default:
+//            break
+//        }
     }
     
     private func choseDiagramImagee(fill: Float, imageView: UIImageView, name: String) {
@@ -110,42 +81,38 @@ class ViewController: UIViewController {
         
         switch fill {
         case 1:
-            guard let unwrapedImage = UIImage(named: name + "100") else { return }
+            guard let unwrapedImage = UIImage(named: name + "01") else { return }
             imageView.image = unwrapedImage
         case 2:
-            guard let unwrapedImage = UIImage(named: name + "90") else { return }
+            guard let unwrapedImage = UIImage(named: name + "02") else { return }
             imageView.image = unwrapedImage
         case 3:
-            guard let unwrapedImage = UIImage(named: name + "80") else { return }
+            guard let unwrapedImage = UIImage(named: name + "03") else { return }
             imageView.image = unwrapedImage
         case 4:
-            guard let unwrapedImage = UIImage(named: name + "70") else { return }
+            guard let unwrapedImage = UIImage(named: name + "04") else { return }
             imageView.image = unwrapedImage
         case 5:
-            guard let unwrapedImage = UIImage(named: name + "60") else { return }
+            guard let unwrapedImage = UIImage(named: name + "05") else { return }
             imageView.image = unwrapedImage
         case 6:
-            guard let unwrapedImage = UIImage(named: name + "50") else { return }
+            guard let unwrapedImage = UIImage(named: name + "06") else { return }
             imageView.image = unwrapedImage
         case 7:
-            guard let unwrapedImage = UIImage(named: name + "40") else { return }
+            guard let unwrapedImage = UIImage(named: name + "07") else { return }
             imageView.image = unwrapedImage
         case 8:
-            guard let unwrapedImage = UIImage(named: name + "30") else { return }
+            guard let unwrapedImage = UIImage(named: name + "08") else { return }
             imageView.image = unwrapedImage
         case 9:
-            guard let unwrapedImage = UIImage(named: name + "20") else { return }
+            guard let unwrapedImage = UIImage(named: name + "09") else { return }
             imageView.image = unwrapedImage
         case 10:
-            guard let unwrapedImage = UIImage(named: name + "05") else { return }
+            guard let unwrapedImage = UIImage(named: name + "10") else { return }
             imageView.image = unwrapedImage
         default:
             break
         }
-    }
-    
-    @IBAction func sliderAction(_ sender: UISlider) {
-        changeImageAction(slider: sender)
     }
 }
 
