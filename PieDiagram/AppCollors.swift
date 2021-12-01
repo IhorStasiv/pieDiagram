2a9efebe0aed47a6e10e257ae2760eabaeb1f8d5
IhorStasiv <igorstasiv96@gmail.com>
2021-12-01 11:08:13 +0200

    add new images

--- PieDiagram/AppCollors.swift
+++ PieDiagram/AppCollors.swift
@@ -26,12 +26,62 @@ enum AppColors {
     // 250 247 255
     case lightGray
     
+    //
+    case greenFriends
+    
+    //
+    case greenWork
+    
+    //
+    case greenGrowth
+    
+    //
+    case turquoise
+    
+    //
+    case lightBlue
+    
+    //
+    case darkOrange
+    
+    //
+    case lightOrange
+    
+    //
+    case pink
+    
+    //
+    case violet
+    
+    //
+    case yellow
+    
     // MARK: - 0x + HEX + alpha(FF = 1)
     
     var rgbaValue: UInt32 {
         switch self {
         case .lightGray:
             return 0xFAF7FFFF
+        case .greenFriends:
+            return 0xC4DF76FF
+        case .greenWork:
+            return 0xA2EBA9FF
+        case .greenGrowth:
+            return 0x63EFB9FF
+        case .turquoise:
+            return 0x58D9CFFF
+        case .lightBlue:
+            return 0x61BDF2FF
+        case .darkOrange:
+            return 0xFF835BFF
+        case .lightOrange:
+            return 0xFFC480FF
+        case .pink:
+            return 0xDD66CCFF
+        case .violet:
+            return 0x7E31F9FF
+        case .yellow:
+            return 0xFDE499FF
         }
     }
 
